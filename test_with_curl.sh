b570bd4d3b64daf44a5bf56398d1bad211b9782e
Ersin Akyuez <eakyuz@gmx.net>
2022-05-30 12:19:50 +0200

    Fetching JWT and using in other curl requests

--- test_with_curl.sh
+++ test_with_curl.sh
@@ -1,4 +1,5 @@
-# Get JWT Authorization Header
+echo Get JWT Authorization Header
+
 curl --location --request POST 'http://127.0.0.1:5000/auth' \
 --header 'Content-Type: application/json' \
 --data-raw '{
@@ -6,18 +7,37 @@ curl --location --request POST 'http://127.0.0.1:5000/auth' \
 	"password" : "admin"
 }'
 
-# Add First Customer
-curl --location --request PUT 'http://127.0.0.1:5000/customer/1' \
---header 'Authorization: JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2NTM4NjE5OTMsImlhdCI6MTY1Mzg2MTY5MywibmJmIjoxNjUzODYxNjkzLCJpZGVudGl0eSI6MX0.7L3Dg12RFWRNQyIqDIimUPYEC0sNwp3rlNFsuKbv9o8' \
+
+
+TOKEN=$(curl --location --request POST 'http://127.0.0.1:5000/auth' \
 --header 'Content-Type: application/json' \
+--data-raw '{
+	"username" : "admin",
+	"password" : "admin"
+}'| jq -r '.access_token'  )
+#echo Token: $TOKEN
+
+
+echo Adding the first customer
+
+curl --location --request PUT 'http://127.0.0.1:5000/customer/1' \
+--header "Authorization: JWT ${TOKEN}" --header 'Content-Type: application/json' \
 --data-raw '{
 	"name":"Ersin Akyuz"
 }'
 
-# Add Second Customer
+
+echo Adding the second customer
 curl --location --request PUT 'http://127.0.0.1:5000/customer/2' \
---header 'Authorization: JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2NTM4NjIxNTgsImlhdCI6MTY1Mzg2MTg1OCwibmJmIjoxNjUzODYxODU4LCJpZGVudGl0eSI6MX0.JOThpp2ILsFq7ae43ms5cHIJhEEUwWDDEGZfdlfwG6c' \
---header 'Content-Type: application/json' \
+--header "Authorization: JWT ${TOKEN}" --header 'Content-Type: application/json' \
+--data-raw '{
+	"name":"John Doe",
+    "is_active" : 1
+}'
+
+echo Adding the second customer in order to test duplication check
+curl --location --request PUT 'http://127.0.0.1:5000/customer/2' \
+--header "Authorization: JWT ${TOKEN}" --header 'Content-Type: application/json' \
 --data-raw '{
 	"name":"John Doe",
     "is_active" : 1
