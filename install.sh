bbdcfe46f57c245d5df3f4a87bc6808146731c15
Ersin Akyuz <eakyuz@ersinakyuz.com>
2022-06-13 07:02:02 +0000

    Install script for Ubuntu

--- install.sh
+++ install.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+if python3 --version ;
+then
+	pip3 install numpy
+	pip3 install flask
+	pip3 install flask_jwt
+	pip3 install flask_restful
+	pip3 install flask_sqlalchemy
 
-pip3 install numpy
-pip3 install flask
-pip3 install flask_jwt
-pip3 install flask_restful
-pip3 install flask_sqlalchemy
+else
+	echo "No Python3 executable is found. Please install Python3 first"
+fi
+
